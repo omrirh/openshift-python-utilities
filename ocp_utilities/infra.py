289d050a4ab03818badfb27bab9006be66a95f18
Omri Bar-Haim <obarhaim@redhat.com>
2023-05-08 13:52:18 +0300

    support for updating pull-secrets
    update pull-secret with new-content passed as env var

--- ocp_utilities/infra.py
+++ ocp_utilities/infra.py
@@ -8,7 +8,10 @@ import kubernetes
 from ocp_resources.image_content_source_policy import ImageContentSourcePolicy
 from ocp_resources.node import Node
 from ocp_resources.resource import ResourceEditor
+<<<<<<< HEAD
 from ocp_resources.secret import Secret
+=======
+>>>>>>> bd1efc8 (support for updating pull-secrets)
 from ocp_wrapper_data_collector.data_collector import (
     get_data_collector_base_dir,
     get_data_collector_dict,
@@ -23,6 +26,7 @@ from ocp_utilities.exceptions import (
     PodsFailedOrPendingError,
 )
 from ocp_utilities.utils import run_command
+from ocp_resources.secret import Secret
 
 
 LOGGER = get_logger(name=__name__)
@@ -359,6 +363,7 @@ def create_icsp(icsp_name, repository_digest_mirrors):
     return icsp
 
 
+<<<<<<< HEAD
 def dict_base64_encode(_dict):
     """
     Encoding dict in base64
@@ -416,3 +421,23 @@ def create_update_secret(secret_data_dict, name, namespace):
     secret.data_dict = {secret_key: dict_base64_encode(_dict=secret_data_dict)}
 
     return secret.deploy()
+=======
+def update_secret(new_content_dict, secret_name, secret_namespace):
+    secret = Secret(name=secret_name, namespace=secret_namespace)
+
+    if secret.exists:
+        secret_data_dict = json.loads(base64.b64decode(secret.instance.data[".dockerconfigjson"]))
+        new_secret_data_dict = secret_data_dict | new_content_dict
+        new_secret_data_encoded = base64.b64encode(str(new_secret_data_dict).encode("ascii")).decode("utf-8")
+        import ipdb
+        ipdb.set_trace()
+        ResourceEditor(patches={secret: {"data": {".dockerconfigjson": new_secret_data_encoded}}}).update()
+    else:
+        secret_data_encoded = base64.b64encode(str(new_content_dict).encode("ascii")).decode("utf-8")
+        with cluster_resource(Secret)(
+            name=secret_name, namespace=secret_namespace, data_dict={".dockerconfigjson": secret_data_encoded}
+        ) as secret:
+            secret.deploy()
+
+    return secret
+>>>>>>> bd1efc8 (support for updating pull-secrets)
